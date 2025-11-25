ba6e45678766863f65860fe650016ff2ae892f25
krittiphong-45 <krittiphong.cpp@gmail.com>
2025-11-25 13:37:08 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const diceDisplay = document.getElementById("diceDisplay")
+const rollButton = document.getElementById("rollButton")
+const historyList = document.getElementById("historyList")
+const noHistoryMessage = document.getElementById("noHistoryMessage")
